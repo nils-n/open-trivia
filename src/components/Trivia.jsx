790b61388bebd7bb4ac4dcfe03fea1ddfe0b6a83
Nils <52067471+nils-n@users.noreply.github.com>
2024-02-23 19:10:10 +0000

    add html-entities fo decoding

--- src/components/Trivia.jsx
+++ src/components/Trivia.jsx
@@ -1,9 +1,11 @@
+import { decode } from "html-entities";
+
 export function Trivia(props) {
   console.log(props.allQuestions);
 
   const questionsEl = props.allQuestions.map((questionData) => {
     console.log(questionData);
-    return <h2> {questionData.question} </h2>;
+    return <h2> {decode(questionData.question)} </h2>;
   });
   console.log("we are in Trivia component now");
   console.log(questionsEl);
