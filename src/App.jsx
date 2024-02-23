d947bddad5a43836c6b758fbc26e1694ebb5f757
Nils <52067471+nils-n@users.noreply.github.com>
2024-02-23 18:33:58 +0000

    add basic API call and set up its mock for development

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { Start } from "./components/Start.jsx";
 import { Trivia } from "./components/Trivia.jsx";
+import tempData from "./tempData.js";
 
 function App() {
   const [startQuiz, setStartQuiz] = useState(false);
+  const [questionData, setQuestions] = useState([]);
+
+  useEffect(() => {
+    // fetch("https://opentdb.com/api.php?amount=5&type=multiple")
+    //   .then((res) => res.json())
+    //   .then((data) => setQuestions((prev) => data.results));
+    const data = tempData;
+    setQuestions(data);
+    console.log(data);
+  }, []);
+
+  // console.log(questions);
 
   function toggleStart() {
     console.log("Start Button clicked");
---
+++ src/tempData.js
@@ -0,0 +1,61 @@
+export default {
+  response_code: 0,
+  results: [
+    {
+      type: "multiple",
+      difficulty: "medium",
+      category: "Entertainment: Video Games",
+      question:
+        "Who is credited with having created the world&#039;s first video game Easter Egg?",
+      correct_answer: "Warren Robinett",
+      incorrect_answers: ["Julius Smith", "Will Crowther", "Don Woods"],
+    },
+    {
+      type: "multiple",
+      difficulty: "medium",
+      category: "History",
+      question: "Which Nazi General was known as the &quot;Desert Fox&quot;?",
+      correct_answer: "Erwin Rommel",
+      incorrect_answers: [
+        "Gerd von Rundstadt",
+        "Wilhelm Keitel",
+        "Heinz Guderian ",
+      ],
+    },
+    {
+      type: "multiple",
+      difficulty: "easy",
+      category: "Entertainment: Cartoon &amp; Animations",
+      question:
+        "In the 1993 Disney animated series &quot;Bonkers&quot;, what is the name of Bonker&#039;s second partner?",
+      correct_answer: "Miranda Wright",
+      incorrect_answers: [
+        "Dick Tracy",
+        "Eddie Valiant",
+        "Dr. Ludwig von Drake",
+      ],
+    },
+    {
+      type: "multiple",
+      difficulty: "medium",
+      category: "General Knowledge",
+      question:
+        "Which of the following Ivy League universities has its official motto in Hebrew as well as in Latin?",
+      correct_answer: "Yale University",
+      incorrect_answers: [
+        "Princeton University",
+        "Harvard University",
+        "Columbia University",
+      ],
+    },
+    {
+      type: "multiple",
+      difficulty: "medium",
+      category: "Entertainment: Television",
+      question:
+        "In the Doctor Who universe, how many times can a time lord normally regenerate?",
+      correct_answer: "12",
+      incorrect_answers: ["11", "13", "15"],
+    },
+  ],
+};
