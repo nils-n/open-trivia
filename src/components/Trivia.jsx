510420d7e43d4cc038438fa51b8f178722e3a903
Nils <52067471+nils-n@users.noreply.github.com>
2024-02-24 06:32:28 +0000

    refactor list of options into unordered list

--- src/components/Trivia.jsx
+++ src/components/Trivia.jsx
@@ -2,16 +2,27 @@ import { decode } from "html-entities";
 
 export function Trivia(props) {
   const questionsEl = props.allQuestions.map((questionData) => {
+    const options = questionData.options.map((option, ix) => {
+      console.log(option);
+      return (
+        <li
+          className="trivia--option"
+          key={ix}
+        >
+          {decode(option)}{" "}
+        </li>
+      );
+    });
+
+    console.log(options);
+
     return (
       <div
         className="trivia--item"
         key={questionData.id}
       >
         <h2 className="trivia--question"> {decode(questionData.question)} </h2>
-        <p className="trivia--option"> {decode(questionData.options[0])} </p>
-        <p className="trivia--option"> {decode(questionData.options[1])} </p>
-        <p className="trivia--option"> {decode(questionData.options[2])} </p>
-        <p className="trivia--option"> {decode(questionData.options[3])} </p>
+        <ul>{options}</ul>
       </div>
     );
   });
