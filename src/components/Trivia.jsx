70bc6234b99dd07bb2c1098821b779067a4dd634
Nils <52067471+nils-n@users.noreply.github.com>
2024-02-24 07:59:44 +0000

    remove logging and add formSubmit handler

--- src/components/Trivia.jsx
+++ src/components/Trivia.jsx
@@ -1,20 +1,15 @@
 import { decode } from "html-entities";
 
 export function Trivia(props) {
-  console.log(props.allQuestions);
   const questionsEl = props.allQuestions.map((questionData) => {
     // create JSX for the option
-    console.log("questionData is ;", questionData);
     const options = questionData.options.map((option, ix) => {
-      console.log("option is ;", option);
-
       return (
-        <li>
+        <li key={`${questionData.id}_option-${ix}`}>
           <input
             type="radio"
             id={`${questionData.id}_option-${ix}`}
             name={`${questionData.id}`}
-            key={`${questionData.id}_option-${ix}`}
             value={decode(option)}
           />
           <label htmlFor={`${questionData.id}_option-${ix}`}>
@@ -35,8 +30,16 @@ export function Trivia(props) {
     );
   });
 
+  function handleSubmit(e) {
+    console.log("form submitted");
+    e.preventDefault();
+  }
+
   return (
-    <form className="trivia--form">
+    <form
+      onSubmit={handleSubmit}
+      className="trivia--form"
+    >
       {questionsEl}
       <button className="btn trivia--btn">Check Answers</button>
     </form>
